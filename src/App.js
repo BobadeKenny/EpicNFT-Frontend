a09441c256eeb747bb6c5120bc6bdf282b77de96
Kenny <kennygbeminiyi@gmail.com>
2021-12-17 05:50:09 +0100

    basic site completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./styles/App.css";
 import twitterLogo from "./assets/twitter-logo.svg";
 import React, { useEffect, useState } from "react";
+import {ethers} from "ethers";
+import myEpicNft from './utils/myEpicNFT.json'
 
 // Constants
 const TWITTER_HANDLE = "_buildspace";
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 const OPENSEA_LINK = "";
 const TOTAL_MINT_COUNT = 50;
+    
+const CONTRACT_ADDRESS = "0x4d1c60c772F2996c5d0422085F245666Ee7E746D";
 
 const App = () => {
   // Render Methods
@@ -24,6 +28,7 @@ const App = () => {
       const account = accounts[0];
       console.log("Authorized account found: ", account);
       setCurrentAccount(account);
+      setupEventListener()
     } else {
       console.log("No account found.");
     }
@@ -31,27 +36,80 @@ const App = () => {
 
   const connectWallet = async () => {
     try {
-      const {ethereum} = window;
+      const { ethereum } = window;
       if (!ethereum) {
         alert("Get Metamask");
         return;
       }
-      const accounts = await ethereum.request({method: "eth_requestAccounts"});
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
       console.log("Connected: ", accounts[0]);
       setCurrentAccount(accounts[0]);
-    } catch(error){
+      setupEventListener()
+    } catch (error) {
       console.log(error);
     }
+  };
+
+  const setupEventListener = async () => {
+    try {
+      const {ethereum} = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        connectedContract.on("NewEpicNFTMinted", (from, tokenId) => {
+          console.log(from, tokenId.toNumber())
+          alert(`NFT minted and sent to wallet. View here: https://rinkeby.rarible.com/token/${CONTRACT_ADDRESS}:${tokenId.toNumber()}`)
+        });
+        console.log("Setup Event Listener")
+      } else {
+        console.log("Eth object doesnt exist")
+      }
+    } catch (error) {
+      console.log(error)
+    }
   }
+  const askContractToMintNft = async () => {
+    try {
+      const {ethereum} = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const connectedContract = new ethers.Contract(CONTRACT_ADDRESS, myEpicNft.abi, signer);
+
+        console.log("Popping wallet now to pay gas")
+        let nftTxn = await connectedContract.makeAnEpicNFT();
+        console.log("Mining")
+        await nftTxn.wait();
+        console.log(`Mined, see transaction: https://rinkeby.etherscan.io/tx/${nftTxn.hash}`);
+      } else {
+        console.log("Eth object doesnt exist")
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, [])
   const renderNotConnectedContainer = () => (
-    <button className="cta-button connect-wallet-button">
+    <button
+      onClick={connectWallet}
+      className="cta-button connect-wallet-button"
+    >
       Connect to Wallet
     </button>
   );
 
-  useEffect(() => {
-    checkIfWalletIsConnected();
-  }, []);
+  const renderMintUI = () => (
+    <button onClick={askContractToMintNft} className="cta-button connect-wallet-button">
+      Mint NFT
+    </button>
+  )
 
   return (
     <div className="App">
@@ -61,7 +119,7 @@ const App = () => {
           <p className="sub-text">
             Each unique. Each beautiful. Discover your NFT today.
           </p>
-          {renderNotConnectedContainer()}
+          {currentAccount === "" ? renderNotConnectedContainer() : renderMintUI()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
